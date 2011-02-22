eca5a644c3ea5710bf402a00f6ba3345153f7d9e
Eric Haskins <Eric Haskins@localhost>
2011-02-21 21:04:11 -0600

    Updated discovery tool.

--- EHaskins.Next.RobotConfiguratorCli/Program.cs
+++ EHaskins.Next.RobotConfiguratorCli/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using MiscUtil.IO;
+using MiscUtil.Conversion;
+using System.IO;
 namespace EHaskins.Next.RobotConfiguratorCli
 {
     class Program
@@ -19,17 +22,32 @@ namespace EHaskins.Next.RobotConfiguratorCli
             {
                 Console.WriteLine("Press enter to attempt connection.");
                 Console.ReadLine();
-                var initBytes = new byte[] { 0x01, 0x00, 0x00 };
+                var initBytes = new byte[] { 0x01, 0x00, 0x00};
                 var epBroad = new IPEndPoint(IPAddress.Broadcast, configPort);
                 client.Send(initBytes, initBytes.Length, epBroad);
 
                 IPEndPoint sourceEp = null;
                 var data = client.Receive(ref sourceEp);
+                var reader = new EndianBinaryReader(new BigEndianBitConverter(), new MemoryStream(data));
+                var id = reader.ReadUInt16();
+                Console.WriteLine("Device ID: " + id);
 
-                for (int i = 0; i < data.Length; i++)
+                var mac = reader.ReadBytes(6);
+                Console.Write("MAC: ");
+                for (int i = 0; i < mac.Length; i++)
                 {
-                    Console.WriteLine(data[i].ToString("x"));
+                    Console.Write(mac[i].ToString("x"));
+                    if (i < 5)
+                    {
+                        Console.Write(":");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
                 }
+
+
             }
             Console.ReadLine();
         }
