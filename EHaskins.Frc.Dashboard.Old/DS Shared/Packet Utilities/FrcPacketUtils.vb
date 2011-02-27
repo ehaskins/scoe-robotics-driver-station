f41a9a912eaa68307df0fdb2ee661074d996dcc8
Eric Haskins <Eric Haskins@localhost>
2011-02-27 01:24:41 -0600

    Horribly broken binding implementation, abanodoning this line, but here for reference.

--- EHaskins.Frc.Dashboard.Old/DS Shared/Packet Utilities/FrcPacketUtils.vb
+++ EHaskins.Frc.Dashboard.Old/DS Shared/Packet Utilities/FrcPacketUtils.vb
@@ -2,40 +2,6 @@
 Imports MiscUtil.Conversion
 
 Public Module FrcPacketUtils
-    'Public Function VerifyFrcCrc(ByVal data As Byte()) As Boolean
-    '    Dim calculatedCrc As UInteger = 0
-    '    Dim dataCrc As UInteger = 0
-    '    Dim crc As New Crc32()
-
-    '    dataCrc = BitConverter.ToUInt32(data.Skip(data.Length - 4).Take(4).ToArray(), 0)
-
-    '    'remove CRC bytes from data before calculating CRC.
-    '    Dim crcData(data.Length - 1) As Byte
-    '    data.Take(data.Length - 4).ToArray.CopyTo(crcData, 0)
-
-    '    calculatedCrc = BitConverter.ToUInt32(crc.ComputeHash(crcData), 0)
-
-    '    Return dataCrc = calculatedCrc
-    'End Function
-
-    'Public Function VerifyFrcCrc(ByVal data As Byte()) As Boolean
-    '    Dim calculatedCrc As UInteger = 0
-    '    Dim dataCrc As UInteger = 0
-
-    '    Using crc As New Crc32()
-    '        dataCrc = BitConverter.ToUInt32(data, data.Length - 4)
-
-    '        'remove CRC bytes from data before calculating CRC.
-    '        For i As Integer = data.Length - 4 To data.Length
-    '            data(i - 1) = 0
-    '        Next
-
-    '        calculatedCrc = BitConverter.ToUInt32(crc.ComputeHash(data), 0)
-
-    '        Return dataCrc = calculatedCrc
-    '    End Using
-    'End Function
-
     Public Function VerifyFrcCrc(ByVal data As Byte()) As Boolean
         'Dim calculatedCrc As UInteger = 0
         Dim dataCrc As UInteger = 0
