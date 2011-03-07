8a35ce0eb756ea396e5b2822889252bcde46c863
Eric Haskins <eric@ehaskins.net>
2011-03-06 20:02:16 -0600

    Cleaning up solution

--- EHaskins.Frc.Dashboard.Old/DS Shared/Alliance.vb
+++
@@ -1,7 +0,0 @@
-Imports System.IO
-
-
-Public Enum Alliance As Byte
-    Red = 82
-    Blue = 66
-End Enum
--- EHaskins.Frc.Dashboard.Old/old/DS Client/IDashboardClientOfT.vb
+++
@@ -1,4 +0,0 @@
-Public Interface IDashboardClient(Of UserDataProcessor As IDashboardDataProcessor)
-    Inherits IDashboardClient
-    Property UserData() As UserDataProcessor
-End Interface
