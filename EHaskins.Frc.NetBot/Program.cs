2c1badd0cf81f023005773d94528de1950c610d8
Eric Haskins <Eric Haskins@localhost>
2011-03-09 15:59:18 -0600

    Playing with netduino

--- EHaskins.Frc.NetBot/Program.cs
+++ EHaskins.Frc.NetBot/Program.cs
@@ -6,15 +6,34 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using Microsoft.SPOT.Net.NetworkInformation;
 
 namespace EHaskins.Frc.NetBot
 {
     public class Program
     {
+        static String ip = "172.16.92.198";
+        static String subnet = "255.255.0.0";
+        static String gateway = "0.0.0.0";
+
         public static void Main()
         {
-            // write your code here
+            int counter = 0;
+            var ifaces = NetworkInterface.GetAllNetworkInterfaces();
 
+            if (ifaces[0].IPAddress != ip)
+            {
+                Debug.Print("Setting IP");
+                ifaces[0].EnableStaticIP(ip, subnet, gateway);
+            }
+            
+            Debug.Print("IP:" + ifaces[0].IPAddress + " Mask:" + ifaces[0].SubnetMask);
+
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
 
         }
 
