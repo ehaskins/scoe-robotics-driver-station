a78117c5e32196bcc8d14cafcc09f7f46d9d3684
Eric Haskins <Eric Haskins@localhost>
2011-03-09 15:59:18 -0600

    Playing with netduino

---
+++ EHaskins.Frc.Communication.Shared/Extensions/SerializationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EHaskins.Frc.Communication.Extensions
+{
+    static class SerializationExtensions
+    {
+        static void LoadFromBytes(this ControlData obj, byte[] data, int offset)
+        {
+                
+        }
+        static byte[] GetBytes(this ControlData obj)
+    {
+    
+        }
+    }
+}
